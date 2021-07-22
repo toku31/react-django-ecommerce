62a97cff290dc36cee040aa5b5651fd1c2e06865
garden627 <garden627@yahoo.co.jp>
2021-07-22 12:20:49 +0900

    Section5 Shopping Cart

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
-import { Row, Col, Image, ListGroup, Button, Card, ListGroupItem } from 'react-bootstrap';
+import { Row, Col, Image, ListGroup, Button, Card, Form } from 'react-bootstrap';
 import Rating from '../components/Rating'
 // import products from '../products'
 // import axios from 'axios'  // 24
@@ -10,11 +10,8 @@ import { listProductDetails } from '../actions/productActions'
 import Loader from '../components/Loader';
 import Message from '../components/Message';
 
-function ProductScreen({ match }) {
-  // const product = products.find((p) => p._id === match.params.id)
-  // console.log(product)
-
-  // const [product, setProduct] = useState([])  // 24 comment out
+function ProductScreen({ match, history }) {   // propsからdestrcturing 分割代入
+  const [qty, setQty] = useState(1)
 
   const dispatch = useDispatch()
   const productDetails = useSelector((state) => state.productDetails);
@@ -23,17 +20,14 @@ function ProductScreen({ match }) {
   useEffect(() => {
 
     dispatch(listProductDetails(match.params.id))
-    // 24
-    // async function fetchProduct(){    
-    //   const {data}= await axios.get(`/api/products/${match.params.id}`)
-    //   setProduct(data)
-    // }
-
-    // fetchProduct()
 
   },[dispatch, match])
 
-  // let product = {}
+  const addToCartHandler = () => {
+    console.log('Add to cart:', match.params.id)
+    history.push(`/cart/${match.params.id}?qty=${qty}`)
+    // props.history.push(`/cart/${productId}?qty=${qty}`)
+  };
 
   return (
     <div>
@@ -87,9 +81,40 @@ function ProductScreen({ match }) {
                           </Col>
                         </Row>
                       </ListGroup.Item>
+
+                      {product.countInStock > 0 && (
+                        <ListGroup.Item>
+                          <Row>
+                            <Col>Qty</Col>
+                            <Col xs='auto' className='my-1'>
+                              <Form.Control
+                                  as="select"
+                                  value={qty}
+                                  onChange={(e)=>setQty(e.target.value)}
+                              >
+                                {
+                                  // [0,1,2]
+                                  [...Array(product.countInStock).keys()].map((x)=>(
+                                    <option key={x+1} value={x+1}>
+                                      { x + 1}
+                                    </option>
+                                  ))
+                                }
+                              </Form.Control>
+                            </Col>
+
+                          </Row>
+                        </ListGroup.Item>
+                      )}
         
                       <ListGroup.Item>
-                        <Button className='btn-block' disabled={product.countInStock===0} type='button' >Add to cart</Button>
+                        <Button 
+                          onClick={addToCartHandler}
+                          className='btn-block' 
+                          disabled={product.countInStock===0} 
+                          type='button' >
+                            Add to cart
+                        </Button>
                       </ListGroup.Item>
         
                     </ListGroup>
