576ff0cdeb1a40c725ca8728656e540a5739a508
garden627 <garden627@yahoo.co.jp>
2021-09-14 23:23:16 +0900

    Section7 Frontend User Authentication

---
+++ frontend/src/actions/userActions.js
@@ -0,0 +1,150 @@
+import axios from "axios";
+import { 
+  USER_REGISTER_FAIL, 
+  USER_REGISTER_REQUEST, 
+  USER_REGISTER_SUCCESS, 
+
+  USER_LOGIN_FAIL, 
+  USER_LOGIN_REQUEST, 
+  USER_LOGOUT, 
+  USER_LOGIN_SUCCESS,
+
+  USER_DETAILS_REQUEST,
+  USER_DETAILS_SUCCESS,
+  USER_DETAILS_FAIL,
+
+  USER_UPDATE_PROFILE_REQUEST,
+  USER_UPDATE_PROFILE_FAIL,
+  USER_UPDATE_PROFILE_SUCCESS,
+  USER_DETAILS_RESET, } from "../constants/userConstants"
+
+export const register = (name, email, password) => async (dispatch) => {
+    try {
+      dispatch({ type: USER_REGISTER_REQUEST});
+
+      const config = {
+        headers: {
+            'Content-type': 'application/json'
+        }
+    }
+
+      const {data} = await axios.post(
+        '/api/users/register/', 
+        { 'name': name, 'email': email, 'password': password },
+        config
+      );
+
+      dispatch({ type: USER_REGISTER_SUCCESS, payload: data })
+      dispatch({ type: USER_LOGIN_SUCCESS, payload: data })
+      localStorage.setItem('userInfo', JSON.stringify(data));
+
+  }catch(error){
+    dispatch({
+      type: USER_REGISTER_FAIL,
+      payload: error.response && error.response.data.detail
+      ? error.response.data.detail
+      : error.message,
+    });
+  };
+}
+
+export const login = (email, password) => async (dispatch) => {
+  try {
+      dispatch({
+          type: USER_LOGIN_REQUEST
+      })
+
+      const config = {
+          headers: {
+              'Content-type': 'application/json'
+          }
+      }
+      
+      const { data } = await axios.post(
+        '/api/users/login/',
+        { 'username': email, 'password': password },
+        config
+    )
+
+    dispatch({ 
+      type: USER_LOGIN_SUCCESS, 
+      payload: data
+     })
+
+    localStorage.setItem('userInfo', JSON.stringify(data));
+
+  }catch(error){
+    dispatch({
+      type: USER_LOGIN_FAIL,
+      payload: error.response && error.response.data.detail
+      ? error.response.data.detail
+      : error.message,
+    });
+  };
+}
+
+export const logout = () => (dispatch) => {
+  localStorage.removeItem('userInfo');
+  // localStorage.removeItem('cartItems');
+  // localStorage.removeItem('shippingAddress');
+  dispatch({ type: USER_LOGOUT});
+  dispatch({ type: USER_DETAILS_RESET});
+}
+
+export const getUserDetails = (id) => async (dispatch, getstate) => {
+  
+  try {
+    dispatch({type: USER_DETAILS_REQUEST, payload: id });
+
+    const { userLogin:{userInfo}} = getstate();
+
+    const config = {
+      headers: {
+          'Content-type': 'application/json',
+          Authorization: `Bearer ${userInfo.token}`
+      }
+    }
+
+    const { data } = await axios.get(
+      `/api/users/${id}`, 
+      config
+    )
+
+    dispatch({type: USER_DETAILS_SUCCESS, payload: data })
+  } catch(error){
+    const message = error.response && error.response.data.message
+    ? error.response.data.message
+    : error.message;
+    dispatch({type: USER_DETAILS_FAIL, payload: message});
+  }
+}
+
+export const updateUserProfile = (user) => async(dispatch, getState ) => {
+  
+  
+  try {
+   dispatch({type: USER_UPDATE_PROFILE_REQUEST});
+
+   const { userLogin: { userInfo }} = getState();
+
+   const config = {
+    headers: {
+        'Content-type': 'application/json',
+        Authorization: `Bearer ${userInfo.token}`
+    }
+  }
+
+   const { data } = await axios.put(`/api/users/profile/update/`, user, config);
+
+   dispatch({type: USER_UPDATE_PROFILE_SUCCESS, payload: data });
+   dispatch({ type: USER_LOGIN_SUCCESS, payload: data});
+   localStorage.setItem('usesrInfo', JSON.stringify(data));
+  } catch(error){
+    dispatch({
+      type: USER_UPDATE_PROFILE_FAIL, 
+      payload: error.response && error.response.data.detail
+      ? error.response.data.detail
+      : error.message,
+    })
+  }
+}
