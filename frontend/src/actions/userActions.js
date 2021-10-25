48325da487d0da1c27557db60991942c2fca179e
garden627 <garden627@yahoo.co.jp>
2021-10-25 21:13:28 +0900

    Section9 Checkout2

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -16,7 +16,10 @@ import {
   USER_UPDATE_PROFILE_REQUEST,
   USER_UPDATE_PROFILE_FAIL,
   USER_UPDATE_PROFILE_SUCCESS,
-  USER_DETAILS_RESET, } from "../constants/userConstants"
+  USER_DETAILS_RESET,
+} from "../constants/userConstants"
+  
+import { ORDER_LIST_MY_RESET } from '../constants/orderConstants'
 
 export const register = (name, email, password) => async (dispatch) => {
     try {
@@ -89,6 +92,7 @@ export const logout = () => (dispatch) => {
   // localStorage.removeItem('shippingAddress');
   dispatch({ type: USER_LOGOUT});
   dispatch({ type: USER_DETAILS_RESET});
+  dispatch({ type: ORDER_LIST_MY_RESET});
 }
 
 export const getUserDetails = (id) => async (dispatch, getstate) => {
--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -4,8 +4,10 @@ import { Link } from 'react-router-dom'
 import { getUserDetails, updateUserProfile } from '../actions/userActions'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
-import { Form, Button, Row, Col } from 'react-bootstrap'
-import { USER_LOGIN_FAIL, USER_UPDATE_PROFILE_RESET } from '../constants/userConstants'
+import { Form, Button, Row, Col, Table } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+import { USER_UPDATE_PROFILE_RESET } from '../constants/userConstants'
+import { listMyOrders } from '../actions/orderActions'
 
 function ProfileScreen({ history }) {
 
@@ -25,6 +27,10 @@ function ProfileScreen({ history }) {
 
       const userUpdateProfile = useSelector(state => state.userUpdateProfile)
       const { success } = userUpdateProfile;
+  
+      const orderListMy = useSelector(state => state.orderListMy)
+      const { loading:loadingOrders, error:errorOrders, orders  } = orderListMy;
+      
             
       useEffect(()=>{
         if (!userInfo){
@@ -33,6 +39,7 @@ function ProfileScreen({ history }) {
           if (!user || !user.name || success){
             dispatch({type:USER_UPDATE_PROFILE_RESET})
             dispatch(getUserDetails('profile'))
+            dispatch(listMyOrders())
           } else {
             setName(user.name);
             setEmail(user.email);
@@ -90,7 +97,42 @@ function ProfileScreen({ history }) {
           </Col>
 
           <Col md={9}>
-          <h2>My Orders</h2>
+            <h2>My Orders</h2>
+            {loadingOrders ? (
+              <Loader/>
+            ) : errorOrders ? (
+              <Message variant='danger'>{errorOrders}</Message>
+            ) : (
+                  <Table striped responsive ClassName='table-sm'>
+                    <thead>
+                      <tr>
+                        <th>ID</th>
+                        <th>Date</th>
+                        <th>Total</th>
+                        <th>Paid</th>
+                        <th>Delivered</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {orders.map(order => (
+                        <tr key={order._id}>
+                          <td>{order._id}</td>
+                          <td>{order.createdAt.substring(0, 10)}</td>
+                          <td>${order.totalPrice}</td>
+                          <td>{order.isPaid ? order.paidAt.substring(0, 10) : (
+                            <i className='fas fa-times' style={{ color: 'red'}}></i>
+                          )}</td>
+                          <td>{order._id}</td>
+                          <td>
+                            <LinkContainer to={`/order/${order._id}`} >
+                              <Button className='btn-sm'>Details</Button>
+                            </LinkContainer>
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </Table>
+            )}
           </Col>
         </Row>
   )
